4fe81f461f2645b4bd4e34f176c8f6c4ec85b54d
tommylenzcarrion-byte <tommy.lenzcarrion@iislagrange.edu.it>
2026-02-26 11:18:36 +0000

    new lenz

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,7 @@
-/*import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router";
 import { PokeAPI } from "./api";
-*/
+
 
 interface Props {
   id: number;
